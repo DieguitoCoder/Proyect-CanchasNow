553a0d1f87eb1e6a867ab621cdb69f8e172c343e
DieguitoCoder <diegot.riwi@gmail.com>
2025-08-31 20:35:38 -0500

    Update db.js
    update credential on database

--- CanchasNow/backend/db.js
+++ CanchasNow/backend/db.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "CanchasNow",
-    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    
 });
+
+module.exports = connection;
