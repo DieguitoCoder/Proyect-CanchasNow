ff8206b0a8094eb5ba6e1a8983a8c25420f5048d
DieguitoCoder <diegot.riwi@gmail.com>
2025-08-31 21:05:14 -0500

    Update db.js

--- CanchasNow/backend/db.js
+++ CanchasNow/backend/db.js
@@ -14,4 +14,14 @@ export const pool = mysql.createPool({
     
 });
 
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
+
+
 export default  connection;
